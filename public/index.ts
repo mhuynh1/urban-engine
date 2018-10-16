5e444465429d4dd16075980cc1564520a3a9396f
Mary Huynh <mhuynhh@gmail.com>
2018-10-16 12:46:57 -0700

    add method to send msg from client to server

--- public/index.ts
+++ public/index.ts
@@ -2,6 +2,22 @@ const serverUrl = 'ws://localhost';
 let io: WebSocket = new WebSocket(serverUrl);
 
 io.onopen = (e: Event): void => {
-    io.send(JSON.stringify({greeting: 'ahoy!'}))
+    io.send(JSON.stringify({ greeting: 'ahoy!' }))
     console.log('socket connected')
+}
+
+const addMessage = (e: Event): false => {
+    const msgData = {
+        username: (document.getElementById('name') as HTMLInputElement).value,
+        body: (document.getElementById('message') as HTMLInputElement).value,
+        likedBy: [],
+        timestamp: Date.now(),
+    }
+    
+    //must send payload as JSON formatted string
+    io.send(JSON.stringify(msgData));
+
+    // empty text input fields
+    (document.getElementById('message') as HTMLInputElement).value = '';
+    return false
 }
